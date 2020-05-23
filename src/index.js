d0ee76ec8f8ddeef9577b6b5a21f47d43a8108cc
Evgeniy <j_savitskii@mail.ru>
2020-05-23 18:55:54 +0300

    Added pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Home, About, Products, Events, Contact } from "./pages";
+import { HashRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/events" component={Events} />
+      <Route path="/products" component={Products} />
+      <Route path="/contact" component={Contact} />
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Home = () => (
+  <section>
+    <h1>[Home Page]</h1>
+  </section>
+);
+
+export const About = () => (
+  <section>
+    <h1>[About the Company]</h1>
+  </section>
+);
+
+export const Products = () => (
+  <section>
+    <h1>[Products Catalog]</h1>
+  </section>
+);
+
+export const Events = () => (
+  <section>
+    <h1>[Events Calendar]</h1>
+  </section>
+);
+
+export const Contact = () => (
+  <section>
+    <h1>[Contact Us]</h1>
+  </section>
+);
